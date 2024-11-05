555a9fdbf33b3aa5228858223306c50c8f3c51c0
gbulat <goran.bulat1987@gmail.com>
2024-11-05 15:27:54 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,8 @@ public class Main {
             System.out.println("i = " + i);
 
             System.out.println("i = " + i);
+            System.out.println("i = " + i);
+            System.out.println("i = " + i);
 
         }
     }
